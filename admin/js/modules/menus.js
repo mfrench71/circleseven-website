9267a324ee1198ace2986719499e4d4d2360e2b8
Matthew French <mfrench71@googlemail.com>
2025-11-28 08:17:57 +0000

    fix(admin): export renderMenuBuilder to window object
    The renderMenuBuilder function was not exported to the window object,
    preventing switchMenuLocation from being able to call it. This caused
    menu items to not render even though data was loaded correctly.

--- admin/js/modules/menus.js
+++ admin/js/modules/menus.js
@@ -683,6 +683,7 @@ export async function saveMenus() {
 
 // Export functions to window for onclick handlers
 window.loadMenus = loadMenus;
+window.renderMenuBuilder = renderMenuBuilder;
 window.switchMenuLocation = switchMenuLocation;
 window.toggleMenuChildren = toggleMenuChildren;
 window.showAddMenuItemModal = showAddMenuItemModal;
